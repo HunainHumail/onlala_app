6568d7ea9161f0d0cb3d37a918b951331bf6d935
HunainHumail <hunainhumail786@gmail.com>
2019-11-13 17:27:27 +0500

    This is mine them bsdk

---
+++ lib/onboarding_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  OnboardingPage ({Key key}) : super(key: key);
+
+  _OnboardingPageState createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+
+    ),
+    );
+
+  }
+}
