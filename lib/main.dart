8bd88051f203cf5914d957404929faa34c9babfd
rajauzairabdullah <Dukandar12!@>
2019-11-14 16:22:52 +0500

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ void main() {
       ),
 
       debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
+      // home: OnboardingPage(),
+      home: SplashScreenOne(),
     )
   );
 }
--- lib/splash_screen_one.dart
+++ lib/splash_screen_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class SplashScreenOne extends StatefulWidget {
   _SplashScreenOneState createState() => _SplashScreenOneState();
@@ -7,61 +8,72 @@ class SplashScreenOne extends StatefulWidget {
 class _SplashScreenOneState extends State<SplashScreenOne> {
   @override
   Widget build(BuildContext context) {
-    
-    AssetImage Logo = AssetImage ('assets/images/logo.png');
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            color: Colors.white,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 210.0,
-              child: Image( image: Logo),
-            ),
-          )
-        ),
-        Expanded(
-          child: Container (
-            color: Color.fromRGBO(21,83,100,50),
-            child: ClipPath(
-              clipper: ClippingClass(),
-              child: Center(
-                child: Text(
-                 "TEST Is hnere ",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                  decoration: TextDecoration.none,
-                  fontSize: 20,
-                  fontFamily: 'Rubik',
-                  ),
-                ),
-              ),
-            ),
-
+    AssetImage Logo = AssetImage('assets/images/logo.png');
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 150.0,
+            backgroundColor: Colors.white,
+            child: Image(image: Logo),
           ),
-
-        ),
-      ],
-
+          Container(
+              height: MediaQuery.of(context).size.height - 185.0,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(61, 102, 127, 1),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 60.0),
+                    child: Center(
+                      child: Text("Welcome to",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 30.0)),
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(top: 30.0, left: 20.0,right: 20.0),
+                      child: Center(
+                        child: Row(
+                          children: <Widget>[
+                            Text('ONLALA',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30.0)),
+                            SizedBox(width: 10.0),
+                            Text('Business Platform',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.white,
+                                    fontSize: 30.0))
+                          ],
+                        ),
+                      )),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0,left: 10.0,right: 10.0),
+                    child: Center(
+                        child: Text('Increase sales prompt business and engage with customers',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                ),
+                                textAlign: TextAlign.center,)),
+                  ),
+                ],
+              ))
+        ],
+      ),
     );
-
   }
-
-}
-
-
-class ClippingClass extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    // TODO: implement getClip
-    return null;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper oldClipper) {
-    // TODO: implement shouldReclip
-    return null;
-  }
-
 }
