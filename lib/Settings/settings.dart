586752cd815c1a34eb14c2690bd46f60feec8949
HunainHumail <hunainhumail786@gmail.com>
2019-11-17 17:02:02 +0500

    Settings Screen Completed (padding added to top)

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -15,48 +15,51 @@ class _Settings extends State<Settings> {
         title: Center(child: Text("Change Password")),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.edit, color: Colors.black, size: 28,),
-                    title: Text('Profile', style: TextStyle(color: Colors.black, fontSize: 17)),
-                    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.edit, color: Colors.black, size: 28,),
+                      title: Text('Profile', style: TextStyle(color: Colors.black, fontSize: 17)),
+                      trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
 
-                  ),
-                  Divider(color: Colors.grey,),
-                  ListTile(
-                    leading: Icon(Icons.notifications, color: Colors.black, size: 28,),
-                    title: Text('Notifications', style: TextStyle(color: Colors.black, fontSize: 17)),
-                    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                    ),
+                    Divider(color: Colors.grey,),
+                    ListTile(
+                      leading: Icon(Icons.notifications, color: Colors.black, size: 28,),
+                      title: Text('Notifications', style: TextStyle(color: Colors.black, fontSize: 17)),
+                      trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
 
-                  ),
-                  Divider(color: Colors.grey,),
-                  ListTile(
-                    leading: Icon(Icons.home, color: Colors.black, size: 28,),
-                    title: Text('Address', style: TextStyle(color: Colors.black, fontSize: 17)),
-                    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                    ),
+                    Divider(color: Colors.grey,),
+                    ListTile(
+                      leading: Icon(Icons.home, color: Colors.black, size: 28,),
+                      title: Text('Address', style: TextStyle(color: Colors.black, fontSize: 17)),
+                      trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
 
-                  ),
-                  Divider(color: Colors.grey,),
-                  ListTile(
-                    leading: Icon(Icons.assignment_turned_in, color: Colors.black, size: 28,),
-                    title: Text('Terms and Conditions', style: TextStyle(color: Colors.black, fontSize: 17)),
-                    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                    ),
+                    Divider(color: Colors.grey,),
+                    ListTile(
+                      leading: Icon(Icons.assignment_turned_in, color: Colors.black, size: 28,),
+                      title: Text('Terms and Conditions', style: TextStyle(color: Colors.black, fontSize: 17)),
+                      trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
 
-                  ),
+                    ),
 
-                ],
-              ),
-            )
+                  ],
+                ),
+              )
 
-          ],
+            ],
 
+          ),
         ),
       ),
     );
